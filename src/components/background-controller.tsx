4aac741292bddc9e3a33a6a00ffc882a1f5aeaba
rajanshresth <shrestharajan726@gmail.com>
2024-05-17 23:03:22 +0545

    Build background-controller component

--- src/components/background-controller.tsx
+++ src/components/background-controller.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { Slider } from "@/components/ui/slider";
+import ColorController from "./color-controller";
+
+const DEFAULT_SIZE = 32;
 
 const BackgroundController = () => {
-  return <div className="bg-slate-400">BackgroundController</div>;
+  const [backgroundSize, setBackgroundSize] = useState(DEFAULT_SIZE);
+  const [backgroundPadding, setBackgroundPadding] = useState(0);
+
+  const [backgroundColor, setBackgroundColor] = useState("rgba(255,255,255,1)"); // [1
+  // Define the type for the slider change event
+  const handleSliderChange = (value: number[]) => {
+    if (value.length > 0) {
+      setBackgroundSize(value[0]);
+    }
+  };
+
+  const handleColorChange = (color: string) => {
+    setBackgroundColor(color);
+  };
+
+  return (
+    <div className="m-4 flex flex-col gap-4 overflow-auto">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="size" className="flex justify-between">
+          Rounded
+          <span>{backgroundSize}px</span>
+        </label>
+        <Slider
+          defaultValue={[DEFAULT_SIZE]}
+          max={512}
+          min={1}
+          step={1}
+          onValueChange={handleSliderChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="rotate" className="flex justify-between">
+          <span>padding</span>
+          <span>{backgroundPadding}px</span>
+        </label>
+        <Slider
+          defaultValue={[0]}
+          max={100}
+          min={0}
+          step={1}
+          onValueChange={(value) => setBackgroundPadding(value[0])}
+        />
+      </div>
+      <ColorController
+        hideController={true}
+        selectedColor={handleColorChange}
+      />
+    </div>
+  );
 };
 
 export default BackgroundController;
