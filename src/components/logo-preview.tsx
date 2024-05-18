d0069508fbc4fcf88d610c084f6cc0c7f036e0c5
rajanshresth <shrestharajan726@gmail.com>
2024-05-18 21:34:21 +0545

    Build download to svg fecture

--- src/components/logo-preview.tsx
+++ src/components/logo-preview.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import { icons } from "lucide-react";
 import { useCombinedContext } from "@/context/combined-context";
 import { IconLucide } from "./icon-list";
+import { Button } from "./ui/button";
 
 type IconName = keyof typeof icons;
 
@@ -17,8 +18,53 @@ const LogoPreview: React.FC = () => {
     iconName,
   } = useCombinedContext();
 
+  const downloadRef = useRef<HTMLDivElement>(null);
+
+  const handleDownloadSVG = () => {
+    if (!downloadRef.current) return;
+    const svgContent = getSVGContent(downloadRef.current);
+    downloadFile(svgContent, "download.svg", "image/svg+xml");
+  };
+
+  const getSVGContent = (element: HTMLElement) => {
+    const width = element.offsetWidth;
+    const height = element.offsetHeight;
+    const html = element.innerHTML;
+
+    const svgOpenTag = `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">`;
+    const svgCloseTag = `</svg>`;
+    const styleTag = `
+      <style>
+        .download-content {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 100%;
+          height: 100%;
+          background-color: ${backgroundColor};
+          border-radius: ${backgroundSize}px;
+        }
+      </style>`;
+    const foreignObjectTag = `<foreignObject width="100%" height="100%">
+      <div xmlns="http://www.w3.org/1999/xhtml" class="download-content">${html}</div>
+    </foreignObject>`;
+
+    return `${svgOpenTag}${styleTag}${foreignObjectTag}${svgCloseTag}`;
+  };
+
+  const downloadFile = (data: string, filename: string, type: string) => {
+    const blob = new Blob([data], { type });
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = filename;
+    link.click();
+    window.URL.revokeObjectURL(url);
+  };
+
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col gap-4 items-center justify-center h-screen">
+      <Button onClick={handleDownloadSVG}>Download</Button>
       <div
         className="h-[400px] w-[400px] bg-gray-200 outline-dotted outline-gray-400"
         style={{
@@ -26,6 +72,8 @@ const LogoPreview: React.FC = () => {
         }}
       >
         <div
+          ref={downloadRef}
+          id="download"
           className="h-full w-full flex items-center justify-center"
           style={{
             background: backgroundColor,
