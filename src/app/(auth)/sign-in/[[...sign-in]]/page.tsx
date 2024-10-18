03c2788f030d3fe8fdcc7300e9b7b9cde9d16f86
rajanshresth <shrestharajan726@gmail.com>
2024-10-18 13:54:45 +0545

    refactor

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,28 +4,28 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 place-content-center h-screen">
-      <div className="flex flex-col gap-4 items-center justify-center">
-        <div className="text-center space-y-4 pt-16">
-          <h1 className="font-bold text-3xl text-gray-800">Welcome Back!</h1>
-          <p className="text-base text-gray-600">
+    <div className='grid grid-cols-1 lg:grid-cols-2 place-content-center h-screen'>
+      <div className='flex flex-col gap-4 items-center justify-center'>
+        <div className='text-center space-y-4 pt-16'>
+          <h1 className='font-bold text-3xl text-gray-800'>Welcome Back!</h1>
+          <p className='text-base text-gray-600'>
             Login or Create an account to get started
           </p>
         </div>
-        <div className="flex items-center justify-center mt-8">
+        <div className='flex items-center justify-center mt-8'>
           <ClerkLoaded>
-            <SignIn path="/sign-in" />
+            <SignIn path='/sign-in' />
           </ClerkLoaded>
           <ClerkLoading>
-            <Loader2 size={32} className="animate-spin text-muted-foreground" />
+            <Loader2 size={32} className='animate-spin text-muted-foreground' />
           </ClerkLoading>
         </div>
       </div>
-      <div className="hidden md:flex lg:flex justify-center items-center">
+      <div className='hidden md:flex lg:flex justify-center items-center'>
         <Image
-          src="/logomakerapp.png"
-          alt="project management logo"
-          className="object-cover"
+          src='/logomakerapp.png'
+          alt='project management logo'
+          className='object-cover rounded-full'
           width={500}
           height={500}
         />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,16 +6,22 @@ import { ModeToggle } from "./mode-toggle";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center ">
-      <div className="flex gap-2 justify-center items-center">
-        <Image src="/logomakerapp.png" alt="logo" width={64} height={64} />
-        <h1 className="font-bold">LOGGO MAKER</h1>
+    <header className='flex justify-between items-center '>
+      <div className='flex gap-2 justify-center items-center'>
+        <Image
+          src='/logomakerapp.png'
+          alt='logo'
+          width={64}
+          height={64}
+          className='rounded-full'
+        />
+        <h1 className='font-bold'>LOGGO MAKER</h1>
       </div>
-      <div className="flex items-center justify-between gap-6">
+      <div className='flex items-center justify-between gap-6'>
         <ModeToggle />
         <SignedOut>
-          <SignInButton mode="modal">
-            <Button size="sm">Sign in</Button>
+          <SignInButton mode='modal'>
+            <Button size='sm'>Sign in</Button>
           </SignInButton>
         </SignedOut>
         <SignedIn>
